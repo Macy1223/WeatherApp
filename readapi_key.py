b23609a663f821fa1d345f0d346eb99bd63890ab
Maurycy <maurycy2323@gmail.com>
2021-01-02 17:41:27 +0100

    Open file

--- readapi_key.py
+++ readapi_key.py
@@ -1,16 +1,3 @@
-# api_key.txt
-
-with(open("api_key.txt")) as file:
-    for line in file:
-        print(line)
-
-
-# def read_api_key()
-
-def sum(a,b):
-    return a+b
-
-a = sum(1,5)
-print(a)
-sum(2,5)
-print(sum(3,5))
+def readapi_key(filename):
+    with(open(filename)) as file:
+        return file.readline()
