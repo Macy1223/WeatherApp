70f3612591f8fd98bddbf3c4c68aff169d144026
nero2310 <nero2310@wp.pl>
2021-01-02 18:07:45 +0100

    Read data from weather api

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print("siema")
-print("Cos")
+from readapi_key import readapi_key
+import requests
 
+api_key = readapi_key("api_key.txt")
 
-print("Dowolne zmiany kappa")
+response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=warsaw&appid={api_key.strip()}")
 
-
-
+temp = response.json()["main"]["temp"]
+print(temp)
