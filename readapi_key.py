3a3904d12b9675e5560ce654a86f103ac7c53e0f
nero2310 <nero2310@wp.pl>
2021-01-02 17:26:34 +0100

    WIP function explanation

--- readapi_key.py
+++ readapi_key.py
@@ -3,3 +3,14 @@
 with(open("api_key.txt")) as file:
     for line in file:
         print(line)
+
+
+# def read_api_key()
+
+def sum(a,b):
+    return a+b
+
+a = sum(1,5)
+print(a)
+sum(2,5)
+print(sum(3,5))
