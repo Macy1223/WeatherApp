a61066b550a552830e7cb047b624051cf5711e38
nero2310 <nero2310@wp.pl>
2021-01-02 18:36:44 +0100

    Print more temp values

--- main.py
+++ main.py
@@ -7,5 +7,7 @@ api_key = readapi_key("api_key.txt")
 response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=warsaw&appid={api_key.strip()}")
 
 temp = response.json()["main"]["temp"]
-print(kel_to_cel(temp))
+feel_temperature = response.json()["main"]["feels_like"]
 
+print(kel_to_cel(temp))
+print(kel_to_cel(feel_temperature))
