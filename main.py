2697f6bc7419f2f97df004487b3d17c905eb0859
Maurycy <maurycy2323@gmail.com>
2021-01-06 18:47:39 +0100

    Weather and name of the city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from readapi_key import readapi_key
 import requests
 from Kalkulator import kel_to_cel
-
 api_key = readapi_key("api_key.txt")
 
 response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=warsaw&appid={api_key.strip()}")
@@ -9,5 +8,9 @@ response = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q=warsa
 temp = response.json()["main"]["temp"]
 feel_temperature = response.json()["main"]["feels_like"]
 
+weather = response.json()["weather"][0]["main"]
+name = response.json()["name"]
 print(kel_to_cel(temp))
 print(kel_to_cel(feel_temperature))
+print(weather)
+print(name)
